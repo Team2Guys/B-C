ed3ff1213789861c70aad67cde8fe1b2f7c3c2f8
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-07-22 05:53:38 -0700

    Card and container components created

--- src/components/Res-usable/Cards/card.tsx
+++ src/components/Res-usable/Cards/card.tsx
@@ -11,15 +11,15 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({ data }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+    <div className="max-w-sm rounded overflow-hidden m-4">
       <img className="w-full" src={data.image} alt={data.heading} />
-      <div className="px-6 py-4">
+      <div className="px-2 py-4">
         <div className="font-bold text-xl mb-2">{data.heading}</div>
         <p className="text-gray-700 text-base">
           {data.paragraph}
         </p>
       </div>      
-      <div className="px-6 pt-4 pb-2">
+      <div className="">
         <CustomButton>{data.buttonText}</CustomButton>
       </div>
     </div>
