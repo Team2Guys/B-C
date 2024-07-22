a2f0c85723510d25cc6d429b7af9f3ff57b32b2d
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-07-22 05:53:38 -0700

    Card and container components created

--- src/components/Res-usable/Button/Button.tsx
+++ src/components/Res-usable/Button/Button.tsx
@@ -10,7 +10,7 @@ const CustomButton: FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className=" text-black font-light text-sm px-2 rounded"
     >
       {children}
     </button>
