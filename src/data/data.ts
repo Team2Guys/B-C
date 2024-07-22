0a21a0d4f98be00cc7051100dc98a9c1140ed49b
MFaad1 <faadsardar123@gmail.com>
2024-07-22 18:43:48 +0500

    adding Heading menues

---
+++ src/data/data.ts
@@ -0,0 +1,12 @@
+export const menuItem= [ 
+    {id:1,MenuName:'Home'},
+    {id:2,MenuName:'Bilnds', dropDown:true},
+    {id:3,MenuName:'Curtains'},
+    {id:4,MenuName:'Shutters'},
+    {id:5,MenuName:'Commercial'},
+    {id:6,MenuName:'Gallery'},
+    {id:7,MenuName:'Installation'},
+    {id:8,MenuName:'About Us'},
+    {id:8,MenuName:'Contact Us'},
+
+]
