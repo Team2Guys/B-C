9c18ce7501138fb933741d55679720deaa785b91
MFaad1 <faadsardar123@gmail.com>
2024-07-22 11:57:41 +0500

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,15 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  colors: {
+    primary:"#40162C"   
+  },
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
