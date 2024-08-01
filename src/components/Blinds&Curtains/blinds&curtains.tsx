bc6df4c02d6ec53082ee3861fe8fee67f5c29f0c
MFaad1 <faadsardar123@gmail.com>
2024-08-01 18:01:46 +0500

    fixes

--- src/components/Blinds&Curtains/blinds&curtains.tsx
+++ src/components/Blinds&Curtains/blinds&curtains.tsx
@@ -11,7 +11,7 @@ interface BlindsAndCurtainsTypesProps {
 const BlindsAndCurtains: FC<BlindsAndCurtainsTypesProps> = ({ data }) => {
   return (
     <div className="flex flex-col md:flex-row items-center bg-white rounded-lg overflow-hidden">
-      <Image  className="w-full md:w-1/2 h-[600px] " src={data.image} alt={data.heading} />
+      <Image  className="w-full md:w-1/2 h-[600px] " width={100} height={100} src={data.image} alt={data.heading} />
       <div className="p-6 md:w-1/2">
         <h2 className="text-2xl font-bold mb-4">{data.heading}</h2>
         <p className="text-gray-700 mb-4">{data.paragraph}</p>
