3c5e71ff1d3784250b3d2f59b2a97082d0690925
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-08-02 12:10:57 +0500

    container and tailwind

--- src/components/Res-usable/header/Header.tsx
+++ src/components/Res-usable/header/Header.tsx
@@ -1,102 +1,98 @@
 'use client';
 import React, { useState } from 'react';
-import { menuItem } from 'data/data';
 import Link from 'next/link';
-import CustomArrows from 'components/slider/Slider';
 import Container from 'components/Res-usable/Container/Container';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from '../../../../public/assets/images/logomain.png';
+import Image from 'next/image';
 
 function Header() {
-  const [Header, setHeader] = useState<string | null>();
-
-  const showMenuHandler = (HeaderMenuName: string) => {
-    setHeader(HeaderMenuName);
-  };
-
   return (
     <>
       <div className="w-full bg-primary">
         <Container>
-          <p className="text-black text-center py-2 text-12  font-medium tracking-[2.9px] leading-loose">
+          <p className="text-black text-center py-2 text-12  font-semibold md:tracking-[4.6px] md:leading-loose">
             We can visit you, take measurements, help select fabrics & install
-            in 1-2 days. Call Dubai 04 252 2025 now or email us on
-            connect@twoguys.ae
+            in 1-2 days. Call Dubai{' '}
+            <Link className="underline font-semibold" href={'tel:04 252 2025'}>
+              04 252 2025
+            </Link>{' '}
+            now or email us on{' '}
+            <Link
+              className="underline font-semibold"
+              href={'mailto:connect@twoguys.ae'}
+            >
+              connect@twoguys.ae
+            </Link>
           </p>
         </Container>
       </div>
-      <nav className="bg-white shadow-md">
-        <Container>
-          <div className="relative flex items-center justify-between h-16">
-            <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex-shrink-0">
-                <img className="h-8 w-auto" src="/logo.png" alt="Logo" />
-              </div>
-              <div className="block sm:ml-6">
-                <div className="flex space-x-4">
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/"
-                  >
-                    Home
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/blinds"
-                  >
-                    Blinds
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/curtains"
-                  >
-                    Curtains
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/shutters"
-                  >
-                    Shutters
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/commercial"
-                  >
-                    Commercial
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/gallery"
-                  >
-                    Gallery
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/estimator"
-                  >
-                    Estimator
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/about-us"
-                  >
-                    About Us
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"'
-                    href="/contact-us"
-                  >
-                    Contact Us
-                  </Link>
-                  <Link
-                    className='className="text-gray-700 hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium'
-                    href="/free-consultation"
-                  >
-                    Free Consultation
-                  </Link>
-                </div>
-              </div>
-            </div>
+      <nav className="bg-lightgrey shadow-md">
+        <Container className="flex items-center justify-evenly  gap-10">
+          <div>
+            <Image width={150} height={150} src={logo} alt="Logo" />
           </div>
+          <div className="flex space-x-5 text-16">
+            <Link
+              className=" px-3 py-2 rounded-md text-sm font-medium"
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className=" px-3 py-2 rounded-md text-sm font-medium"
+              href="/blinds"
+            >
+              Blinds
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/curtains"
+            >
+              Curtains
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/shutters"
+            >
+              Shutters
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/commercial"
+            >
+              Commercial
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/gallery"
+            >
+              Gallery
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/estimator"
+            >
+              Estimator
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/about-us"
+            >
+              About Us
+            </Link>
+            <Link
+              className="  px-3 py-2 rounded-md text-sm font-medium"
+              href="/contact-us"
+            >
+              Contact Us
+            </Link>
+          </div>
+          <Link
+            className="  px-3 py-2 rounded-md text-sm font-medium bg-primary-foreground"
+            href="/free-consultation"
+          >
+            Free Consultation
+          </Link>
         </Container>
       </nav>
     </>
