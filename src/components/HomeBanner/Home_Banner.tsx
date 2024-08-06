f6f35039962f24a5eca067485c5cdbf6a258f0b7
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-08-05 23:10:47 -0700

    home page banner

--- src/components/HomeBanner/Home_Banner.tsx
+++ src/components/HomeBanner/Home_Banner.tsx
@@ -13,7 +13,7 @@ type BannerProps = {
 
 const Banner: React.FC<BannerProps> = ({ data }) => {
   return (
-    <div className="relative flex items-center justify-start w-full h-56 overflow-hidden bg-gray-200 md:h-72">
+    <div className="relative flex items-center justify-start w-full h-56 overflow-hidden bg-gray-200 md:h-80">
       <Image
         src={data.imageUrl}
         alt="Shutters"
@@ -21,8 +21,8 @@ const Banner: React.FC<BannerProps> = ({ data }) => {
         objectFit="cover"
         className="opacity-90"
       />
-      <div className="absolute w-3/5 inset-y-0 right-0 bg-[#F6EFE9] lg:pl-40 text-left clip-custom-shape p-8 lg:p-20">
-        <h2 className="text-4xl text-left font-bold text-gray-800 lg:pr-20">
+      <div className="absolute lg:w-3/5 inset-y-0 right-0 bg-[#F6EFE9] lg:pl-40 text-left clip-custom-shape lg:p-20 p-20 md:p-20">
+        <h2 className="lg:text-4xl md:text-2xl text-lg text-left font-bold text-gray-800 lg:pr-20 lg:w-3/4 w-3/4">
           {data.title}
         </h2>
         <button className="mt-4 px-6 py-2 text-dark text-left bg-white rounded-lg w-fit ">
