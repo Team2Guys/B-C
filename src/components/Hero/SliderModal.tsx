7cc3cfbd716c8530de92ab74125767950f5bf794
MFaad1 <faadsardar123@gmail.com>
2024-08-10 14:05:40 +0500

    fixes

--- src/components/Hero/SliderModal.tsx
+++ src/components/Hero/SliderModal.tsx
@@ -18,7 +18,7 @@ function SliderModal({setshowModel,className}:SliderModal) {
             </div>
 
             <div id="modalHandler" className='w-full h-full mb-4 text-center text-14 leading-9	 '>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </div>
 
             <div id="modalHandler" className='header-review text-center'>
