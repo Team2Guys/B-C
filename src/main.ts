42d1ac4128787be18ac5b815a9002817f23e1cf0
MFaad1 <faadsardar123@gmail.com>
2024-08-10 14:57:51 +0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MyLoggerService } from './my-logger/my-logger.service';
 import { AllExceptionsFilter } from './all-exceptions.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 
 async function bootstrap() {
@@ -9,11 +11,17 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost) 
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
 
+  app.setGlobalPrefix('api');
+
   app.setGlobalPrefix('docs');
+  const config = new DocumentBuilder()
+  .setTitle('Blind and curtains')
+  .setDescription('Docs for blind and curtains')
+  .setVersion('1.0')
+  .addTag('BNC')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
 
   await app.listen(3200);
 }
 bootstrap();
-
-
-
