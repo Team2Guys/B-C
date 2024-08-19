5c8a4b77bea4b1fcbd70a928dca0208e12b05e2c
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-08-19 06:21:53 -0700

    product guarantee page update

--- src/app/product-guarantees/page.tsx
+++ src/app/product-guarantees/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import TopHero from 'components/ui/top-hero';
-import { productData, relativeProducts } from 'data/data';
+import { productData, ProductGuarantees, relativeProducts } from 'data/data';
 import second from '../../../public//assets/images/product-guarantees/large.png';
 import React from 'react';
 import Container from 'components/Res-usable/Container/Container';
@@ -21,90 +21,38 @@ const productGuarantees = () => {
           </p>
         </div>
       </Container>
-      <Container className="pt-10 max-w-screen-2xl lg:pb-14 flex justify-between lg:gap-10 items-start flex-col md:flex-row">
-        <div className="w-full md:w-1/2 lg:pr-32 px-5">
-          <h3 className="font-bold text-xl xs:text-2xl tracking-wider">
-            OUR 3-YEAR GUARANTEE
-          </h3>
-          <p className="text-16 xs:text-18 leading-8 mt-4 text-lightdark">
-            When choosing blindsandcurtains.ae for your window dressings, you
-            can relax in the knowledge that you’re not only buying a beautiful
-            product, but you’re also buying into a customer service ethos that
-            will be like no other experience in Dubai. From our after-sales care
-            package to our 1 working day guaranteed response, you can rest
-            assured that you’ll be in good hands for the lifetime of your
-            blinds, curtains or shutters. Great care and pride is attached into
-            everything we do. From the blinds we supply and our beautiful
-            handmade curtains, to the shutters that we import. You’ll have our
-            support from the day they are installed. This is the promise we make
-            to you. When choosing blindsandcurtains.ae for your window
-            dressings, you can relax in the knowledge that you’re not only
-            buying a beautiful product, but you’re also buying into a customer
-            service ethos that will be like no other experience in Dubai. From
-            our after-sales care package to our 1 working day guaranteed
-            response, you can rest assured that you’ll be in good hands for the
-            lifetime of your blinds, curtains or shutters. Great care and pride
-            is attached into everything we do. From the blinds we supply and our
-            beautiful handmade curtains, to the shutters that we import. You’ll
-            have our support from the day they are installed. This is the
-            promise we make to you.
-          </p>
-          <div className="h-fit mt-8"></div>
-        </div>
-        <div className="w-full md:w-1/2 ">
-          <Image
-            src={productData.sideImage1}
-            width={1000}
-            height={300}
-            alt="why-us img"
-            className="mx-auto md:me-0 ms-auto w-full"
-          />
-        </div>
-      </Container>
-      <Container className="lg:pt-5 pt-8 max-w-screen-2xl lg:pb-14 flex justify-between lg:gap-10 items-start flex-col md:flex-row">
-        <div className="w-full md:w-1/2 lg:order-1 order-2">
-          <Image
-            src={productData.sideImage}
-            width={1000}
-            height={300}
-            alt="why-us img"
-            className="mx-auto md:me-0 ms-auto w-full"
-          />
-        </div>
-        <div className="w-full md:w-1/2 lg:pl-20 px-5 lg:order-2 order-1">
-          <h3 className="font-bold text-xl xs:text-2xl tracking-wider">
-            OUR LIFETIME WARRANTY
-          </h3>
-          <p className="text-16 xs:text-18 leading-8 mt-4 text-lightdark ">
-            From the day your order is fitted, you will have a fully
-            comprehensive 3-year Guarantee against manufacturing defects. This
-            encompasses all parts, components, and materials used. Once the 3
-            years have passed, you can relax in the knowledge that we will be
-            there to support you throughout the lifetime of your blinds,
-            curtains or shutters. Where repairs are required, we will provide
-            this service for you at a nominal fee and cost of goods only. Where
-            goods are no longer available, we will offer you the most
-            cost-effective method of resolving any issues. When
-            choosing blindsandcurtains.ae for your window dressings, you can
-            relax in the knowledge that you’re not only buying a beautiful
-            product, but you’re also buying into a customer service ethos that
-            will be like no other experience in Dubai. From our after-sales care
-            package to our 1 working day guaranteed response, you can rest
-            assured that you’ll be in good hands for the lifetime of your
-            blinds, curtains or shutters. Great care and pride is attached into
-            everything we do. From the blinds we supply and our beautiful
-            handmade curtains, to the shutters that we import. You’ll have our
-            support from the day they are installed. This is the promise we make
-            to you.
-          </p>
-          <div className="h-fit mt-8"></div>
-        </div>
-      </Container>
+      {ProductGuarantees.map((data: any, index: any) => (
+        <Container
+          key={index}
+          className={`pt-10 max-w-screen-2xl lg:pb-14 flex justify-between lg:gap-10 items-start flex-col md:flex-row ${data.imageAlign === 'right' ? 'lg:flex-row-reverse' : ''}`}
+        >
+          <div
+            className={`w-full md:w-1/2 ${data.imageAlign === 'right' ? 'lg:pl-20' : 'lg:pr-32'} px-5`}
+          >
+            <h3 className="font-bold text-xl xs:text-2xl tracking-wider">
+              {data.heading}
+            </h3>
+            <p className="text-16 xs:text-18 leading-8 mt-4 text-lightdark">
+              {data.text}
+            </p>
+            <div className="h-fit mt-8"></div>
+          </div>
+          <div
+            className={`w-full md:w-1/2 ${data.imageAlign === 'right' ? 'lg:order-1' : 'lg:order-2'}`}
+          >
+            <Image
+              src={data.image}
+              width={1000}
+              height={300}
+              alt="why-us img"
+              className="mx-auto md:me-0 ms-auto w-full"
+            />
+          </div>
+        </Container>
+      ))}
       <Container className="py-10">
         <RelatedProducts products={relativeProducts} />
       </Container>
-      {/* <BookNowBanner />
-      <OurClient /> */}
     </>
   );
 };
