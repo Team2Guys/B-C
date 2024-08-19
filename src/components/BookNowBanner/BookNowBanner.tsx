f53f95248150b20f74b0da278782f6afdb845194
wajid-fareed <wajidfareed103@gmail.com>
2024-08-19 16:53:16 +0500

    appointment page

--- src/components/BookNowBanner/BookNowBanner.tsx
+++ src/components/BookNowBanner/BookNowBanner.tsx
@@ -17,14 +17,14 @@ const BookNowBanner = () => {
     >
       <div className="absolute w-full h-full bg-light opacity-60"></div>
       <Container className="z-0">
-        <p className="lg:text-4xl text-2xl font-medium mx-auto w-fit text-center">
+        <p className="text-2xl md:text-3xl lg:text-4xl font-medium mx-auto w-fit text-center">
           Need some more information or to{' '}
           <span className="font-black">Arrange an Appointment?</span>
           <br />
           Send us an email or call us on: 04 252 2025
         </p>
         <div className="mt-6 text-center">
-          <Link href="/" className="px-4 py-3 bg-white rounded-md">
+          <Link href="/appointment" className="px-4 py-3 bg-white rounded-md">
             Booking Now
           </Link>
         </div>
