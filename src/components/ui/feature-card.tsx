3a8abfca32ea4d711f670dc16b41c6c8f01e03e0
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-08-30 15:22:35 +0500

    pull mian

--- src/components/ui/feature-card.tsx
+++ src/components/ui/feature-card.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
-import { FeatureProductData } from 'types/interface';
+import { PRODUCTS_TYPES } from 'types/interface';
 
 interface FeatureCardProps {
-  products: FeatureProductData[];
+  products: PRODUCTS_TYPES[];
 }
 
 const FeatureCard: React.FC<FeatureCardProps> = ({ products }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 max-w-screen-2xl mx-auto px-2">
       {products.map((product) => (
-        <div key={product.id} className="relative group w-full">
+        <div key={product._id} className="relative group w-full">
           <div className="absolute w-full bottom-0">
             <div className="bg-white flex justify-between items-center w-full p-2 px-4 opacity-0 group-hover:opacity-100 duration-700">
-              <p className="text-12 lg:text-16">{product.title}</p>
+              <p className="text-12 lg:text-16">{product.name}</p>
               <Link
-                href={product.link}
+                href="/"
                 className="border border-primary rounded-md px-1 lg:px-2 py-1 hover:bg-primary hover:text-white text-12 lg:text-14"
               >
                 View More
@@ -27,8 +27,8 @@ const FeatureCard: React.FC<FeatureCardProps> = ({ products }) => {
             width={450}
             height={450}
             className="md:object-contain w-full h-full"
-            src={product.image}
-            alt={product.title}
+            src={product.posterImageUrl}
+            alt={product.name}
           />
         </div>
       ))}
