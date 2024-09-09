4ab67eb5720d180d24e57db911a94584d88c9ff9
MFaad1 <faadsardar123@gmail.com>
2024-09-09 15:33:11 +0500

    new changes added

--- src/components/ui/Product-Card.tsx
+++ src/components/ui/Product-Card.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { ProductCardData } from 'types/interface';
+import { ProductCardData } from 'types/interfaces';
 import { IProduct } from 'types/types';
 
 interface ProductCardDataProps {
