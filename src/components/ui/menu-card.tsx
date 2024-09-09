ccf900222bdc468c97da565ac6aedaba51c4c42d
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-09-09 11:37:07 +0500

    products page fixe appointment fix

--- src/components/ui/menu-card.tsx
+++ src/components/ui/menu-card.tsx
@@ -17,11 +17,11 @@ const MenuCard: React.FC<MenuCardProps> = ({ src, alt, title, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="space-y-2 hover:text-black cursor-pointer"
+      className="space-y-2 hover:text-black cursor-pointer flex flex-col items-center"
     >
-      <Image width={600} height={600} src={src} alt={alt} />
+      <Image className='w-[120px] h-[120px] rounded-lg' width={600} height={600} src={src} alt={alt} />
       <div>
-        <p className="text-15 font-normal text-center">{title}</p>
+        <p className="text-15 font-normal ">{title}</p>
       </div>
     </div>
   );
