dc5bfe65e3e2d4b3f9188fa2eeee59c3bab05702
MFaad1 <faadsardar123@gmail.com>
2024-09-10 16:47:07 +0500

    fixes

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -151,11 +151,11 @@ const ECommerce: React.FC = () => {
             )} */}
       </div>
 
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+      {/* <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
         <ChartThree />
-      </div>
+      </div> */}
     </>
   );
 };
