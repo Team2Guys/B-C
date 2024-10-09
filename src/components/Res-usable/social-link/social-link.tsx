aef5212f13333b67327e574e2789e89d9f987b03
Wajid-Fareed <wajidfareed103@gmail.com>
2024-10-09 16:45:24 +0500

    comment section created

--- src/components/Res-usable/social-link/social-link.tsx
+++ src/components/Res-usable/social-link/social-link.tsx
@@ -7,7 +7,7 @@ const SocialLink: React.FC = () => {
   return (
     <div className="flex gap-3 items-center">
       {SocialData.map((social, index) => (
-        <Link target="_blank" href={social.href} key={index}>
+        <Link target="_blank" href={social.href ? social.href: ''} key={index}>
           <Image className='w-[28px] h-[28px]' width={40} height={40} src={social.src} alt={social.alt} />
         </Link>
       ))}
