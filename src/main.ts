4186d356ea52060307186d598ac1cabf4755464e
MFaad1 <faadsardar123@gmail.com>
2024-11-13 15:28:28 +0500

    new changes added

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 
-
-
-
-
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
+      "http://localhost:3001",
       /\.vercel\.app$/
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true
   })
 
   const { httpAdapter } = app.get(HttpAdapterHost) 
