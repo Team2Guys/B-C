98d7e8994bbe9917df751b452238332689b98ed9
misbahkhalil07 <misbahperveenpaf@gmail.com>
2024-11-14 14:17:06 +0500

    homepage content updated

--- src/components/Res-usable/guarrenty/guarrenty.tsx
+++ src/components/Res-usable/guarrenty/guarrenty.tsx
@@ -9,12 +9,10 @@ const Guarrenty = () => {
       <Container className="grid grid-cols-1 lg:grid-cols-2 max-w-screen-2xl mx-auto">
         <div className="py-4 pe-4 lg:py-12 lg:pe-12 2xl:py-24 2xl:pe-24 lg:px-0 px-4 space-y-4">
           <h2 className="text-20 2xl:text-[43px] font-bold">
-            OUR LIFETIME GUARANTEE
+          Fully Guaranteed For Your Peace Of Mind
           </h2>
           <p className=" text-14 xl:text-16 leading-4 text-start ">
-            All our made-to-measure blinds, curtains and shutters come fully
-            guaranteed, so you can rest assured you’re investing in quality
-            products that are made to the highest specifications.
+          All our made-to-measure shutters, blinds and curtains come with a 10-year warranty on all hardware and 5-year warranty on all fabrics. You can rest assured you’re investing in quality products.
           </p>
           <Link href={'/product-guarantees'} className="mt-4 block">
             <Button className="shadow-slate-400 shadow-md w-32" variant={'white'}>
