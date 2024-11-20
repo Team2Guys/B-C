7be59aa10a087ae6fa24e77f7b589412cb9a41bc
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-11-20 16:42:44 +0500

    pull main

--- src/components/ui/Product-Card.tsx
+++ src/components/ui/Product-Card.tsx
@@ -26,17 +26,25 @@ const ProductCard: React.FC<ProductCardDataProps> = ({
     queryKey: ['categories'],
     queryFn: fetchSubCategories,
   });
+
+  const getTrimmedTitle = (title: string) => {
+    return title.replace(/^Made to measure\s+/i, ''); 
+  };
+
   return (
     <div
       className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${isSizeSmall && 'lg:grid-cols-4'} gap-5 p-1 md:p-0 mt-5`}
     >
       {products &&
-        products.map((product) => {
+        products.map((product: IProduct) => {
           const category = categories?.find(
             (cat) => cat.id === product.CategoryId,
           );
           if (!category) return null;
+
+          const trimmedProductTitle = getTrimmedTitle(product.title);
           const parent = generateSlug(category?.title);
+
           return (
             <div
               className="border group rounded-xl border-white hover:border-primary p-3 space-y-3 text-center pb-10"
@@ -50,7 +58,7 @@ const ProductCard: React.FC<ProductCardDataProps> = ({
                 alt="img"
               />
               <div className="text-center space-y-3">
-                <h1 className="text-17 font-semibold">{product.title}</h1>
+                <h1 className="text-17 font-semibold">{trimmedProductTitle}</h1>
                 <p className="text-15 font-light md:w-[80%] mx-auto auto max-h-16 overflow-y-auto custom-scrollbar">
                   {product.short_description || product.description}
                 </p>
@@ -64,7 +72,7 @@ const ProductCard: React.FC<ProductCardDataProps> = ({
                   }}
                   className="bg-transparent border border-white group-hover:bg-primary group-hover:border-primary text-black group-hover:text-white py-3 px-5 rounded-md"
                 >
-                  View {category?.title ? category.title : ''}
+                  View {category.title}
                 </button>
               </div>
             </div>
