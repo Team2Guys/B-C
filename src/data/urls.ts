6aac12dd0bdbc9cfe0e7f42ca85b007ea4063187
MFaad1 <faadsardar123@gmail.com>
2024-11-21 17:46:03 +0500

    fixes

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -5,3 +5,17 @@ export const urls = [
         Url : "blinds-for-kids-room"
     }
     ]
+
+
+
+
+export  const ChangedProductUrl = (title: string): string => {
+
+    let products = urls.find((url: { productName: string, Url: string }) => {
+      return (url.Url === title)
+    })
+
+    return products ? products.productName :title
+
+
+  }
