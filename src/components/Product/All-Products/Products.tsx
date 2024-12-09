54241e30a9d170379c847a0eff88cd97b1f9cf78
misbahkhalil07 <misbahperveenpaf@gmail.com>
2024-12-09 17:09:33 +0500

    update content

--- src/components/Product/All-Products/Products.tsx
+++ src/components/Product/All-Products/Products.tsx
@@ -2,6 +2,7 @@
 import Container from 'components/Res-usable/Container/Container';
 import { Button } from 'components/ui/button';
 import ProductCard from 'components/ui/Product-Card';
+import { categorydata } from 'data/data';
 import {
   extendedByRoom,
   extendedByStyle,
@@ -11,6 +12,7 @@ import {
   megaMenubyStyle,
   megaMenuDynamic,
 } from 'data/data';
+import { usePathname } from 'next/navigation';
 import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { IProduct } from 'types/types';
@@ -21,10 +23,12 @@ interface relativeProps {
 }
 
 const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
-  const [activeCategory, setActiveCategory] = useState<string>('All');
+  const pathname = usePathname();
+  const [activeCategory, setActiveCategory] = useState<string>('ALL');
+  const [content, setContent] = useState({ title: '', subtitle: '' });
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [productsPerPage, setProductsPerPage] = useState<number>(8);
-  const categories = ['All', 'By Style', 'By Room', 'dynamic'];
+  const categories = ['ALL', 'By Style', 'By Room', 'By Function'];
   const productContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -47,68 +51,45 @@ const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
       window.removeEventListener('resize', updateProductsPerPage);
     };
   }, []);
-  const byRoomItems = [...extendedByRoom, ...megaMenubyRoom].flat();
-  const ByRoomItems = useMemo(
-    () =>
-      products.filter((product) =>
-        byRoomItems.some(
-          (item) => item.productName === generateSlug(product.title),
-        ),
-      ),
-    [products, byRoomItems],
-  );
 
-  const byStyleItems = [...extendedByStyle, ...megaMenubyStyle].flat();
-  const ByStyleItems = useMemo(
-    () =>
-      products.filter((product) =>
-        byStyleItems.some(
-          (item) => item.productName === generateSlug(product.title),
-        ),
-      ),
-    [products, byStyleItems],
-  );
+  useEffect(() => {
+    const mainCategory = categorydata.find(
+      (category) => category.category.toLowerCase() === categoryType.toLowerCase()
+    );
 
-  const byDynamic = [...extendedDynamic, ...megaMenuDynamic].flat();
-  const ByDynamicItems = useMemo(
-    () =>
-      products.filter((product) =>
-        byDynamic.some(
-          (item) => item.productName === generateSlug(product.title),
-        ),
-      ),
-    [products, byDynamic],
-  );
+    if (mainCategory) {
+      const categoryContent = mainCategory.types.find(
+        (type) => type.type.toLowerCase() === activeCategory.toLowerCase()
+      );
+
+      if (categoryContent) {
+        setContent({
+          title: categoryContent.title,
+          subtitle: categoryContent.subtitle,
+        });
+      } else {
+        setContent({ title: '', subtitle: '' });
+      }
+    } else {
+      setContent({ title: '', subtitle: '' });
+    }
+  }, [activeCategory, categoryType]);
 
   const filteredProducts: IProduct[] = useMemo(() => {
     switch (activeCategory) {
-      case 'All':
+      case 'ALL':
         return products;
-      case 'By Room':
-        return ByRoomItems;
-      case 'By Style':
-        return ByStyleItems;
-      case 'dynamic':
-        return ByDynamicItems;
+      // Add filters for By Style, By Room, and By Function here if needed
       default:
         return products;
     }
-  }, [
-    activeCategory,
-    ByRoomItems,
-    ByStyleItems,
-    ByStyleItems,
-    ByDynamicItems,
-    products,
-  ]);
-  // const finalFilteredProducts = activeCategory === 'By Type' ? ByTypeItems.reverse() : filteredProducts;
-  console.log('================================================');
-  console.log(filteredProducts);
+  }, [activeCategory, products]);
+
   const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
   const startIndex = (currentPage - 1) * productsPerPage;
   const visibleProducts = filteredProducts.slice(
     startIndex,
-    startIndex + productsPerPage,
+    startIndex + productsPerPage
   );
 
   const handlePageChange = (page: number) => {
@@ -116,7 +97,6 @@ const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
     if (productContainerRef.current) {
       productContainerRef.current.scrollIntoView({ behavior: 'smooth' });
 
-      // Calculate the position to scroll to (subtracting 100 pixels)
       const offset = 200; // Adjust this value as needed
       const top =
         productContainerRef.current.getBoundingClientRect().top +
@@ -130,46 +110,36 @@ const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
   return (
     <Container className="mt-10 md:mt-16">
       <div className="mt-10">
-        <div className="flex lg:gap-10 gap-3 justify-center whitespace-nowrap overflow-x-auto ">
-          {categories.map((category) => {
-            const parent = generateSlug(categoryType);
-            return (
-              <Button
-                key={category}
-                className={`text-15 font-bold
-                ${activeCategory === category ? 'bg-secondary text-white px-2 md:px-8 py-2 md:py-7' : 'text-black bg-transparent px-2 md:px-8 py-2 md:py-7'}`}
-                onClick={() => {
-                  setActiveCategory(category);
-                  setCurrentPage(1);
-                }}
-              >
-                {category === 'dynamic'
-                  ? parent === 'blinds'
-                    ? 'By Function'
-                    : parent === 'curtains'
-                      ? 'By Fabrics'
-                      : parent === 'shutters'
-                        ? 'By Colour'
-                        : 'By Design'
-                  : category}
-              </Button>
-            );
-          })}
+        <div className="flex lg:gap-10 gap-3 justify-center whitespace-nowrap overflow-x-auto">
+          {categories.map((category) => (
+            <Button
+              key={category}
+              className={`text-15 font-bold ${
+                activeCategory === category
+                  ? 'bg-secondary text-white px-2 md:px-8 py-2 md:py-7'
+                  : 'text-black bg-transparent px-2 md:px-8 py-2 md:py-7'
+              }`}
+              onClick={() => {
+                setActiveCategory(category);
+                setCurrentPage(1);
+              }}
+            >
+              {category}
+            </Button>
+          ))}
         </div>
         <hr className="h-2 mt-5 md:mt-8 border-black" />
         <div className="mt-10 text-center space-y-3">
           <h1 className="text-[#231F20] text-20 md:text-24 lg:text-[36px] font-semibold uppercase">
-            Explore Popular Made to Measure {categoryType ? categoryType : ''}{' '}
-            Options
+            {content.title}
           </h1>
           <p className="text-14 md:text-15 font-normal md:w-[65%] mx-auto">
-            Our bespoke blinds are designed to meet your needs. Various types of
-            window blinds are available in a variety of materials and colours,
-            so you can create an ambience to suit your style.
+            {content.subtitle}
           </p>
         </div>
+
         <div ref={productContainerRef} className="my-2" />
-        <div className="" id="productContainer">
+        <div id="productContainer">
           <ProductCard products={visibleProducts} isSizeSmall={true} />
         </div>
 
@@ -183,17 +153,19 @@ const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
             >
               <FaArrowLeft size={16} />
             </Button>
-
             {Array.from({ length: totalPages }, (_, index) => (
               <Button
                 key={index}
-                className={`w-[55px] h-[55px] text-16 ${currentPage === index + 1 ? 'bg-secondary text-white' : 'bg-transparent text-black'}`}
+                className={`w-[55px] h-[55px] text-16 ${
+                  currentPage === index + 1
+                    ? 'bg-secondary text-white'
+                    : 'bg-transparent text-black'
+                }`}
                 onClick={() => handlePageChange(index + 1)}
               >
                 {index + 1}
               </Button>
             ))}
-
             <Button
               variant={'secondary'}
               className="w-[55px] h-[55px] bg-transparent text-black hover:bg-secondary hover:text-white text-16"
@@ -210,3 +182,4 @@ const AllProducts: React.FC<relativeProps> = ({ products, categoryType }) => {
 };
 
 export default AllProducts;
+
