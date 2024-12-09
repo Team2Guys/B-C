a4a74ce7476ad0c218ea8c0738f7d3ab8c19617a
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-12-09 10:27:03 +0500

    Update: Sub-Category pages

--- src/data/Images.ts
+++ src/data/Images.ts
@@ -1,4 +1,14 @@
-export const Categories_wise_Images = [
+interface ICategories_wise_Images {
+  Category_id: number;
+  sub_Category: string;
+  Product: {
+    product_name: string;
+    altText: string;
+    Imagesurl: string;
+    desc: string;
+  }[];
+}
+export const Categories_wise_Images: ICategories_wise_Images[] = [
   {
     Category_id: 2,
     sub_Category: 'Bedroom Blinds',
@@ -8,24 +18,28 @@ export const Categories_wise_Images = [
         altText: 'Roman Blinds',
         Imagesurl:
           '/assets/images/optimized-images/bedroom-blinds/Roman-Blinds-for-bedroom.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Blackout/Private Blinds',
         altText: 'Blackout/Private Blinds',
         Imagesurl:
           '/assets/images/optimized-images/bedroom-blinds/Roller-Blinds-for-Bedroom.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Wooden Blinds',
         altText: 'Wooden Blinds',
         Imagesurl:
           '/assets/images/optimized-images/bedroom-blinds/Wooden-blinds-for-bedroom.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Blackout Roller Blinds',
         altText: 'Blackout Roller Blinds',
         Imagesurl:
           '/assets/images/optimized-images/bedroom-blinds/Blackout-blinds-for-bedroom.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -37,21 +51,25 @@ export const Categories_wise_Images = [
         product_name: 'Roman Blinds',
         altText: 'Roman Blinds',
         Imagesurl: '/assets/images/optimized-images/Staircase Blinds/r.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Honeycomb Blinds',
         altText: 'Honeycomb Blinds',
         Imagesurl: '/assets/images/optimized-images/Staircase Blinds/c.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Sunscreen/Transparent',
         altText: 'Sunscreen/Transparent',
         Imagesurl: '/assets/images/optimized-images/Staircase Blinds/s.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Aluminium Blinds',
         altText: 'Aluminium Blinds',
         Imagesurl: '/assets/images/optimized-images/Staircase Blinds/a.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -63,21 +81,25 @@ export const Categories_wise_Images = [
         product_name: 'Skylight blinds dubai',
         altText: 'Skylight blinds dubai',
         Imagesurl: '/assets/images/optimized-images/Conservatory/s.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Vertical Blinds',
         altText: 'Vertical Blinds',
         Imagesurl: '/assets/images/optimized-images/Conservatory/v.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Wooden Blinds',
         altText: 'Wooden Blinds',
         Imagesurl: '/assets/images/optimized-images/Conservatory/w.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Dimout Blinds/Semi Transparent',
         altText: 'Dimout Blinds/Semi Transparent',
         Imagesurl: '/assets/images/optimized-images/Conservatory/d.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -90,24 +112,28 @@ export const Categories_wise_Images = [
         altText: 'Sheer Horizon Blinds',
         Imagesurl:
           '/assets/images/optimized-images/Blinds for Kids Room/s.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Day/Night Blinds',
         altText: 'Day/Night Blinds',
         Imagesurl:
           '/assets/images/optimized-images/Blinds for Kids Room/d.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Transculent Blinds',
         altText: 'Transculent Blinds',
         Imagesurl:
           '/assets/images/optimized-images/Blinds for Kids Room/t.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Blackout Roller Blinds',
         altText: 'Blackout Roller Blinds',
         Imagesurl:
           '/assets/images/optimized-images/Blinds for Kids Room/b.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -119,21 +145,25 @@ export const Categories_wise_Images = [
         product_name: 'Roman Blinds',
         altText: 'Roman Blinds',
         Imagesurl: '/assets/images/optimized-images/Study room/r.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Patricia Blinds',
         altText: 'Patricia Blinds',
         Imagesurl: '/assets/images/optimized-images/Study room/p.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Wooden Blinds',
         altText: 'Wooden Blinds',
         Imagesurl: '/assets/images/optimized-images/Study room/w.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Blackout Roller Blinds',
         altText: 'Blackout Roller Blinds',
         Imagesurl: '/assets/images/optimized-images/Study room/b.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -145,16 +175,19 @@ export const Categories_wise_Images = [
         product_name: 'Blackout Blinds',
         altText: 'Blackout Blinds',
         Imagesurl: '/assets/images/optimized-images/Bathroom Blinds/b.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Vertical Blinds',
         altText: 'Vertical Blinds',
         Imagesurl: '/assets/images/optimized-images/Bathroom Blinds/v.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Skylight blinds dubai',
         altText: 'Skylight blinds dubai',
         Imagesurl: '/assets/images/optimized-images/Bathroom Blinds/s.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -166,16 +199,19 @@ export const Categories_wise_Images = [
         product_name: '3d blinds',
         altText: '3d blinds',
         Imagesurl: '/assets/images/optimized-images/dinnng room/3d.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Aluminium Blinds',
         altText: 'Aluminium Blinds',
         Imagesurl: '/assets/images/optimized-images/dinnng room/a.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Zebra Blinds',
         altText: 'Zebra Blinds',
         Imagesurl: '/assets/images/optimized-images/dinnng room/z.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -187,16 +223,19 @@ export const Categories_wise_Images = [
         product_name: 'Aluminium Blinds',
         altText: 'Aluminium Blinds',
         Imagesurl: '/assets/images/optimized-images/Kitchen Blinds/a.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Wooden Blinds',
         altText: 'Wooden Blinds',
         Imagesurl: '/assets/images/optimized-images/Kitchen Blinds/w.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Vertical Blinds',
         altText: 'Vertical Blinds',
         Imagesurl: '/assets/images/optimized-images/Kitchen Blinds/v.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -209,6 +248,7 @@ export const Categories_wise_Images = [
         altText: 'Eton Blinds',
         Imagesurl:
           '/assets/images/optimized-images/Office Window Blinds/Office window blinds.webp',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
@@ -220,21 +260,25 @@ export const Categories_wise_Images = [
         product_name: 'Wooden Blinds',
         altText: 'Eton Blinds',
         Imagesurl: '/assets/images/optimized-images/Living Room Blinds/w.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Roman Blinds',
         altText: 'Roman Blinds',
         Imagesurl: '/assets/images/optimized-images/Living Room Blinds/r.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Vertical Blinds',
         altText: 'Vertical Blinds',
         Imagesurl: '/assets/images/optimized-images/Living Room Blinds/v.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
       {
         product_name: 'Dimout Blinds/Semi Transparent',
         altText: 'Dimout Blinds/Semi Transparent',
         Imagesurl: '/assets/images/optimized-images/Living Room Blinds/rb.jpg',
+        desc: 'Choosing Custom Window Blinds means finding a balance of style, functionality, and ease that adapts perfectly to any space.',
       },
     ],
   },
