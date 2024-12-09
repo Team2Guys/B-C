978118f2377354bf57c66de2c3ed64539785742b
MFaad1 <faadsardar123@gmail.com>
2024-12-09 15:49:45 +0500

    fixes

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,22 +1,3 @@
-// import DefaultLayout from 'components/Dashboard/Layouts/DefaultLayout';
-// import Product from './Product';
-
-// const Produc_page = async () => {
-//   const response = await fetch(
-//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/products/GetAllProducts`,
-//     { next: { tags: ['calculatePrices'] } },
-//   );
-
-//   const products = await response.json();
-
-//   return (
-//     <DefaultLayout>
-//       <Product products={products} />
-//     </DefaultLayout>
-//   );
-// };
-
-// export default Produc_page;
 
 import DefaultLayout from 'components/Dashboard/Layouts/DefaultLayout';
 import Product from './Product';
