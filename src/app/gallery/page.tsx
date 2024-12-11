fc1d7d7c7eb86027a57c3267de4b3d8216e964e0
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-12-11 18:13:41 +0500

    snag fixes

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -132,20 +132,7 @@ const GalleryPage = () => {
         <div className="flex justify-center items-center mt-20 w-full">
           {filteredProducts && filteredProducts.length > 0 && (
             <>
-              <span
-                className={`mx-1 w-16 h-8 md:h-14 flex justify-center items-center font-medium cursor-pointer ${
-                  currentPage === 1
-                    ? 'opacity-0'
-                    : 'hover:bg-btnclr hover:text-white opacity-100'
-                }`}
-                onClick={() =>
-                  setCurrentPage(
-                    currentPage > 1 ? currentPage - 1 : currentPage,
-                  )
-                }
-              >
-                <GoArrowLeft size={25} />
-              </span>
+       
 
               {Array.from({ length: totalPages }, (_, page) => (
                 <button
@@ -163,20 +150,7 @@ const GalleryPage = () => {
                 </button>
               ))}
 
-              <span
-                className={`mx-1 w-16 h-8 md:h-14 flex justify-center items-center font-medium cursor-pointer ${
-                  currentPage === totalPages
-                    ? 'opacity-0'
-                    : 'hover:bg-btnclr hover:text-white opacity-100'
-                }`}
-                onClick={() =>
-                  setCurrentPage(
-                    currentPage < totalPages ? currentPage + 1 : currentPage,
-                  )
-                }
-              >
-                <GoArrowRight size={25} />
-              </span>
+             
             </>
           )}
         </div>
