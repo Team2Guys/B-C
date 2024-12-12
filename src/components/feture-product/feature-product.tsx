af912305003072ba92e6ab1d83228e7d36cb5a74
Wajid-Fareed <wajidfareed103@gmail.com>
2024-12-12 10:12:19 +0500

    ui fixes

--- src/components/feture-product/feature-product.tsx
+++ src/components/feture-product/feature-product.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'components/Res-usable/Container/Container';
 import { Button } from 'components/ui/button';
 import FeatureCard from 'components/ui/feature-card';
@@ -32,7 +32,7 @@ const FeatureProduct: React.FC = () => {
   const [visibleCount, setVisibleCount] = useState<number>(8);
   const categoryOrder = ['All', 'Blinds', 'Curtains', 'Shutters', 'Commercial'];
   const categoryMap = categories?.reduce(
-    (acc, category) => {
+    (acc: any, category: ICategory) => {
       acc[category?.title] = category;
       return acc;
     },
@@ -47,37 +47,62 @@ const FeatureProduct: React.FC = () => {
   const visibleProducts = filteredProducts?.slice(0, visibleCount);
 
   const handleViewMore = () => {
-    setVisibleCount((prevCount) => prevCount + 8);
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 767) {
+      setVisibleCount((prevCount) => prevCount + 8);
+    } else if (screenWidth > 439) {
+      setVisibleCount((prevCount) => prevCount + 4);
+    } else {
+      setVisibleCount((prevCount) => prevCount + 3);
+    }
   };
 
   const handleShowAll = () => {
     setActiveCategory(null);
-    setVisibleCount(8);
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 767) {
+      setVisibleCount(8);
+    } else if (screenWidth > 439) {
+      setVisibleCount(4);
+    } else {
+      setVisibleCount(3);
+    }
   };
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 767) {
+      setVisibleCount(8);
+    } else if (screenWidth > 439) {
+      setVisibleCount(4);
+    } else {
+      setVisibleCount(3);
+    }
+  },[]);
 
   if (isLoadingCategories || isLoadingProducts)
     return (
       <div>
-      <Container className="py-12">
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 gap-3">
-            {[...Array(3)].map((_, index) => (
-              <div className='' key={index}>
+        <Container className="py-12">
+          <div className="flex justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 gap-3">
+              {[...Array(3)].map((_, index) => (
+                <div className='' key={index}>
                   <div className="h-6 bg-gray-300 rounded mb-2"></div>
-                <div
-                
-                className="max-w-md rounded lg:m-4 m-2 animate-pulse"
-                >
-                <div className="bg-gray-300 lg:w-[460px] xl:w-[350px] md:w-[400px] sm:w-full lg:h-[400px] md:h-[300px] sm:h-auto rounded-3xl"></div>
-                <div className="px-2 py-4">
+                  <div
+
+                    className="max-w-md rounded lg:m-4 m-2 animate-pulse"
+                  >
+                    <div className="bg-gray-300 lg:w-[460px] xl:w-[350px] md:w-[400px] sm:w-full lg:h-[400px] md:h-[300px] sm:h-auto rounded-3xl"></div>
+                    <div className="px-2 py-4">
+                    </div>
+                  </div>
                 </div>
-              </div>
-                </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
     );
 
   // Error handling
@@ -95,28 +120,28 @@ const FeatureProduct: React.FC = () => {
       </div>
 
       <div className="mt-10">
-        <div className="overflow-x-auto border">
-        <div className="flex lg:gap-10 gap-3 justify-center whitespace-nowrap min-w-[470px]">
-          {categoryOrder.map((categoryTitle) => {
-            const category = categoryMap?.[categoryTitle];
-            return (
-              <Button
-                key={categoryTitle}
-                variant={'feature'}
-                className={` ${activeCategory?.id === category?.id ? 'bg-secondary text-white' : 'text-black'} px-4 py-6`}
-                onClick={() => {
-                  if (categoryTitle === 'All') {
-                    handleShowAll();
-                  } else {
-                    setActiveCategory(category ?? null);
-                  }
-                }}
-              >
-                {categoryTitle}
-              </Button>
-            );
-          })}
-        </div>
+        <div className="border">
+          <div className="flex lg:gap-10 gap-3 justify-center whitespace-nowrap">
+            {categoryOrder.map((categoryTitle) => {
+              const category = categoryMap?.[categoryTitle];
+              return (
+                <Button
+                  key={categoryTitle}
+                  variant={'feature'}
+                  className={` ${activeCategory?.id === category?.id ? 'bg-secondary text-white' : 'text-black'} px-4 py-6`}
+                  onClick={() => {
+                    if (categoryTitle === 'All') {
+                      handleShowAll();
+                    } else {
+                      setActiveCategory(category ?? null);
+                    }
+                  }}
+                >
+                  {categoryTitle}
+                </Button>
+              );
+            })}
+          </div>
         </div>
         <div className="mt-5">
           <FeatureCard products={visibleProducts || []} />
