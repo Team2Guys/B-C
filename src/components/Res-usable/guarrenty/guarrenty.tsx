9bdbe77f2b055adb8febeff5aae941875a3c6a01
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-12-12 09:45:17 +0500

    link issue and text center in mobile fixed

--- src/components/Res-usable/guarrenty/guarrenty.tsx
+++ src/components/Res-usable/guarrenty/guarrenty.tsx
@@ -7,11 +7,11 @@ const Guarrenty = () => {
   return (
     <div className="bg-light mt-10 justify-items-center">
       <Container className="grid grid-cols-1 lg:grid-cols-2 max-w-screen-2xl mx-auto">
-        <div className="py-4 pe-4 lg:py-12 lg:pe-12 2xl:py-24 2xl:pe-24 lg:px-0 px-4 space-y-4 lg:text-start md:text-start sm:text-start md: xsm:text-center">
+        <div className="py-4 pe-4 lg:py-12 lg:pe-12 2xl:py-24 2xl:pe-24 lg:px-0 px-4 space-y-4 lg:text-start md:text-start sm:text-start md: xsm:text-center text-center">
           <h2 className="text-20 2xl:text-[43px] font-bold ">
           Fully Guaranteed For Your Peace Of Mind
           </h2>
-          <p className=" text-14 xl:text-16 leading-4 lg:text-start md:text-start sm:text-start md: xsm:text-center">
+          <p className=" text-14 xl:text-16 leading-4 lg:text-start md:text-start sm:text-start md: xsm:text-center text-center">
           All our made-to-measure shutters, blinds and curtains come with a 10-year warranty on all hardware and 5-year warranty on all fabrics. You can rest assured you’re investing in quality products.
           </p>
           <Link href={'/product-guarantees'} className="mt-4 block">
