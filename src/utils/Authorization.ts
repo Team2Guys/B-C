a63d30bc79553bf42b8a9cec9077f34cbcd07276
MFaad1 <faadsardar123@gmail.com>
2024-12-20 10:39:07 +0500

    migerated db

--- src/utils/Authorization.ts
+++ src/utils/Authorization.ts
@@ -6,7 +6,6 @@ import * as jwt from 'jsonwebtoken';
 export class JwtMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['authorization']?.split(' ')[1];
-    
     if (!token) {
       throw new UnauthorizedException('Token not provided');
     }
