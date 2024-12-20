d0cec7e1fde246a206e732476607cc274ff439e1
Wajid-Fareed <wajidfareed103@gmail.com>
2024-12-20 15:57:52 +0500

    fixes

--- src/components/HomeBanner/Home_Banner.tsx
+++ src/components/HomeBanner/Home_Banner.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { BannerProps } from 'types/interfaces';
@@ -12,15 +13,15 @@ const Banner: React.FC<BannerProps> = () => {
           alt="Shutters"
           width={800}
           height={600}
-          className="px-2 sm:px-0 h-full md:h-[377px] w-full"
+          className="px-2 h-full md:h-[377px] w-full"
         />
       </div>
       <div className="col-span-1"/>
       <div className="col-span-12 sm:col-span-6 md:col-span-5 2xl:col-span-4 space-y-2 px-4 md:px-0 pt-3 pb-2 sm:py-2 md:py-0 text-center sm:text-start">
         <p className=' text-16 md:text-[34px] xl:text-[41px] font-bold'>From Bay Windows to Patio Doors..</p>
-        <button onClick={() => route.push('/request-appointment')} className="text-16 xs:text-18 leading-4  text-black bg-white py-2 px-4 md:px-6 md:py-4 rounded-md capitalize">
+        <Link href='/request-appointment' className="text-16 xs:text-18 leading-4 block w-fit shadow-md text-black bg-white py-2 px-4 md:px-6 md:py-4 rounded-md capitalize">
         Book A Free Home Design Visit
-        </button>
+        </Link>
       </div>
       <div className="col-span-1"/>
 
