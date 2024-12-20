1007669f5e315e0f3cda3adba68657febffdf833
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-12-20 20:31:02 +0500

    fixes: Build Error ✨

--- src/components/Res-usable/Cards/card.tsx
+++ src/components/Res-usable/Cards/card.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import { ICategory } from 'types/types';
 import Link from 'next/link';
 
