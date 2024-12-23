551668da7fe3f61c2ffdc558555e74425d6ecbbe
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-12-23 10:15:18 +0500

    fixes: Header active issue 🌱

--- src/components/Res-usable/header/Header.tsx
+++ src/components/Res-usable/header/Header.tsx
@@ -10,10 +10,7 @@ import { RiMenuFoldLine } from 'react-icons/ri';
 import SocialLink from '../social-link/social-link';
 import { useQuery } from '@tanstack/react-query';
 import { ICategory, IProduct } from 'types/types';
-import {
-  fetchProducts,
-  fetchSubCategories,
-} from 'config/fetch';
+import { fetchProducts, fetchSubCategories } from 'config/fetch';
 import {
   blindMegaMenuItems,
   commercialMegaMenuItems,
@@ -53,20 +50,12 @@ const Header = () => {
     setDrawerOpen(false);
   };
 
-
-
-
-  const {
-    data: products,
-    error: productsError,
-  } = useQuery<IProduct[]>({
+  const { data: products, error: productsError } = useQuery<IProduct[]>({
     queryKey: ['products'],
     queryFn: fetchProducts,
   });
 
-  const {
-    data: subCategories,
-  } = useQuery<ICategory[]>({
+  const { data: subCategories } = useQuery<ICategory[]>({
     queryKey: ['fetchSubCategories'],
     queryFn: fetchSubCategories,
   });
@@ -77,12 +66,12 @@ const Header = () => {
     <>
       <div className="w-full bg-secondary">
         <Container className="flex flex-wrap md:flex-nowrap gap-2 md:gap-4 justify-center md:justify-between items-center min-h-12 pb-0">
-        <div></div>
+          <div></div>
           <p className="text-white py-2 text-10 sm:text-12 2xl:text-15 font-medium tracking-[4px] leading-relaxed 2xl:leading-loose text-center md:text-start">
             We can visit you, take measurements, help select fabrics & install
             in 2-3 days.
           </p>
-          <div className='hidden md:block'>
+          <div className="hidden md:block">
             <SocialLink />
           </div>
         </Container>
@@ -103,10 +92,11 @@ const Header = () => {
           <div className="w-3/12 lg:w-9/12 mt-9">
             <div className="hidden lg:flex justify-evenly items-start lg:text-10 text-12 xl:text-16 whitespace-nowrap ">
               <Link
-                className={`lg:text-10 text-12 xl:text-15 px-1 transition-all duration-200 ${path === '/'
-                  ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary hover:bg-secondary hover:text-white hover:pb-10 hover:rounded-none'
-                  : 'hover:bg-secondary hover:text-white pb-10 pt-1 px-4'
-                  }`}
+                className={`lg:text-10 text-12 xl:text-15 px-1 transition-all duration-200 ${
+                  path === '/'
+                    ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary hover:bg-secondary hover:text-white hover:pb-10 hover:rounded-none'
+                    : 'hover:bg-secondary hover:text-white pb-10 pt-1 px-4'
+                }`}
                 href={'/'}
               >
                 Home
@@ -119,17 +109,18 @@ const Header = () => {
 
                 let filteredProducts =
                   products?.filter(
-                    (product : IProduct) => product.CategoryId === link.id,
+                    (product: IProduct) => product.CategoryId === link.id,
                   ) || [];
 
                 let combinedSliderData: any[] = [];
 
                 if (link.id === 2) {
-                  const actualProducts = filteredProducts.filter((product: IProduct) =>
-                    blindMegaMenuItems.some(
-                      (menuItem) =>
-                        menuItem.productName === generateSlug(product.title),
-                    ),
+                  const actualProducts = filteredProducts.filter(
+                    (product: IProduct) =>
+                      blindMegaMenuItems.some(
+                        (menuItem) =>
+                          menuItem.productName === generateSlug(product.title),
+                      ),
                   );
 
                   combinedSliderData = [
@@ -138,11 +129,12 @@ const Header = () => {
                   ];
                 }
                 if (link.id === 9) {
-                  const actualProducts = filteredProducts.filter((product: IProduct) =>
-                    shutterMegaMenuItems.some(
-                      (menuItem) =>
-                        menuItem.productName === generateSlug(product.title),
-                    ),
+                  const actualProducts = filteredProducts.filter(
+                    (product: IProduct) =>
+                      shutterMegaMenuItems.some(
+                        (menuItem) =>
+                          menuItem.productName === generateSlug(product.title),
+                      ),
                   );
 
                   combinedSliderData = [
@@ -151,11 +143,12 @@ const Header = () => {
                   ];
                 }
                 if (link.id === 5) {
-                  const actualProducts = filteredProducts.filter((product: IProduct) =>
-                    curtainMegaMenuItems.some(
-                      (menuItem) =>
-                        menuItem.productName === generateSlug(product.title),
-                    ),
+                  const actualProducts = filteredProducts.filter(
+                    (product: IProduct) =>
+                      curtainMegaMenuItems.some(
+                        (menuItem) =>
+                          menuItem.productName === generateSlug(product.title),
+                      ),
                   );
 
                   combinedSliderData = [
@@ -188,28 +181,33 @@ const Header = () => {
                 const isActive =
                   !isBlogPath && path?.includes(generateSlug(link.label));
 
+                const isBalconyActive =
+                  path?.includes('blinds-and-curtains') ||
+                  path?.includes('blinds-curtains');
+
                 return combinedSliderData.length > 0 ? (
                   <MegaMenu
-
                     onClick={handleCloseDrawer}
-
                     key={link.id}
                     title={link.label || ''}
                     sliderData={combinedSliderData}
                     href={link.href}
                     className={
-                      isBlogActive || isActive
+                      link.label === 'Commercial' && isBalconyActive
                         ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary mb-8 hover:mb-0 hover:bg-secondary hover:text-white hover:pb-9 hover:rounded-none'
-                        : 'hover:bg-secondary hover:text-white pb-9 pt-1 px-2 2xl:px-4'
+                        : !isBalconyActive && (isBlogActive || isActive)
+                          ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary mb-8 hover:mb-0 hover:bg-secondary hover:text-white hover:pb-9 hover:rounded-none'
+                          : 'hover:bg-secondary hover:text-white pb-9 pt-1 px-2 2xl:px-4'
                     }
                   />
                 ) : (
                   <Link
                     key={index}
-                    className={`lg:text-10 text-12 xl:text-15 px-1 transition-all duration-200 ${isBlogActive || isActive
-                      ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary hover:bg-secondary hover:text-white hover:pb-10 hover:rounded-none'
-                      : 'hover:bg-secondary hover:text-white pb-10 pt-1 px-2 2xl:px-4'
-                      }`}
+                    className={`lg:text-10 text-12 xl:text-15 px-1 transition-all duration-200 ${
+                      isBlogActive || isActive
+                        ? 'font-bold px-2 2xl:px-4 py-1 rounded-md text-white bg-secondary hover:bg-secondary hover:text-white hover:pb-10 hover:rounded-none'
+                        : 'hover:bg-secondary hover:text-white pb-10 pt-1 px-2 2xl:px-4'
+                    }`}
                     onClick={handleCloseDrawer}
                     href={link.href}
                   >
@@ -237,18 +235,30 @@ const Header = () => {
               >
                 <div className="flex flex-col gap-2">
                   <Link
-                    className={`py-0 text-14 hover:text-black border-b-2 border-white hover:border-b-secondary w-fit font-medium ${path === '/' ? 'border-b-secondary' : ''
-                      }`}
+                    className={`py-0 text-14 hover:text-black border-b-2 border-white hover:border-b-secondary w-fit font-medium ${
+                      path === '/' ? 'border-b-secondary' : ''
+                    }`}
                     onClick={handleCloseDrawer}
                     href="/"
                   >
                     Home
                   </Link>
-                  <Collapse bordered={false} expandIcon={({ isActive }) => isActive ? <IoMdArrowDropup size={20} /> : <IoMdArrowDropdown size={20} />} className='custom-collapse border-0 bg-transparent flex flex-col gap-2'>
+                  <Collapse
+                    bordered={false}
+                    expandIcon={({ isActive }) =>
+                      isActive ? (
+                        <IoMdArrowDropup size={20} />
+                      ) : (
+                        <IoMdArrowDropdown size={20} />
+                      )
+                    }
+                    className="custom-collapse border-0 bg-transparent flex flex-col gap-2"
+                  >
                     {links.map((link, index) => {
                       let filteredSubCategories =
                         subCategories?.filter(
-                          (subcategory: any) => subcategory.CategoryId === link.id,
+                          (subcategory: any) =>
+                            subcategory.CategoryId === link.id,
                         ) || [];
 
                       let filteredProducts =
@@ -259,11 +269,13 @@ const Header = () => {
                       let combinedSliderData: any[] = [];
 
                       if (link.id === 2) {
-                        const actualProducts = filteredProducts.filter((product: IProduct) =>
-                          blindMegaMenuItems.some(
-                            (menuItem) =>
-                              menuItem.productName === generateSlug(product.title),
-                          ),
+                        const actualProducts = filteredProducts.filter(
+                          (product: IProduct) =>
+                            blindMegaMenuItems.some(
+                              (menuItem) =>
+                                menuItem.productName ===
+                                generateSlug(product.title),
+                            ),
                         );
 
                         combinedSliderData = [
@@ -272,11 +284,13 @@ const Header = () => {
                         ];
                       }
                       if (link.id === 9) {
-                        const actualProducts = filteredProducts.filter((product: IProduct) =>
-                          shutterMegaMenuItems.some(
-                            (menuItem) =>
-                              menuItem.productName === generateSlug(product.title),
-                          ),
+                        const actualProducts = filteredProducts.filter(
+                          (product: IProduct) =>
+                            shutterMegaMenuItems.some(
+                              (menuItem) =>
+                                menuItem.productName ===
+                                generateSlug(product.title),
+                            ),
                         );
 
                         combinedSliderData = [
@@ -285,11 +299,13 @@ const Header = () => {
                         ];
                       }
                       if (link.id === 5) {
-                        const actualProducts = filteredProducts.filter((product: IProduct) =>
-                          curtainMegaMenuItems.some(
-                            (menuItem) =>
-                              menuItem.productName === generateSlug(product.title),
-                          ),
+                        const actualProducts = filteredProducts.filter(
+                          (product: IProduct) =>
+                            curtainMegaMenuItems.some(
+                              (menuItem) =>
+                                menuItem.productName ===
+                                generateSlug(product.title),
+                            ),
                         );
 
                         combinedSliderData = [
@@ -302,7 +318,8 @@ const Header = () => {
                           products?.filter((product: IProduct) =>
                             commercialMegaMenuItems.some(
                               (menuItem) =>
-                                menuItem.productName === generateSlug(product.title),
+                                menuItem.productName ===
+                                generateSlug(product.title),
                             ),
                           ) || [];
 
@@ -323,9 +340,23 @@ const Header = () => {
                         !isBlogPath && path?.includes(generateSlug(link.label));
 
                       return combinedSliderData.length > 0 ? (
-                        <Panel key={index} header={<Link href={link.href} onClick={handleCloseDrawer} className={`border-b-2 border-transparent hover:text-black ${isBlogActive || isActive
-                          ? 'border-b-secondary'
-                          : 'hover:border-b-secondary'}`}>{link.label}</Link>} className='custom-panel py-0'>
+                        <Panel
+                          key={index}
+                          header={
+                            <Link
+                              href={link.href}
+                              onClick={handleCloseDrawer}
+                              className={`border-b-2 border-transparent hover:text-black ${
+                                isBlogActive || isActive
+                                  ? 'border-b-secondary'
+                                  : 'hover:border-b-secondary'
+                              }`}
+                            >
+                              {link.label}
+                            </Link>
+                          }
+                          className="custom-panel py-0"
+                        >
                           <MegaMenu
                             onClick={handleCloseDrawer}
                             key={link.id}
@@ -342,10 +373,11 @@ const Header = () => {
                       ) : (
                         <Link
                           key={index}
-                          className={`w-fit text-14 border-b-2 border-white hover:text-black font-medium ${isBlogActive || isActive
-                            ? 'border-b-secondary'
-                            : 'hover:border-b-secondary'
-                            }`}
+                          className={`w-fit text-14 border-b-2 border-white hover:text-black font-medium ${
+                            isBlogActive || isActive
+                              ? 'border-b-secondary'
+                              : 'hover:border-b-secondary'
+                          }`}
                           onClick={handleCloseDrawer}
                           href={link.href}
                         >
@@ -359,7 +391,7 @@ const Header = () => {
             </div>
           </div>
         </Container>
-        <Container className='sm:hidden pb-1 pt-2 text-center'>
+        <Container className="sm:hidden pb-1 pt-2 text-center">
           <Link
             className="py-2 px-6 rounded-md text-14 whitespace-nowrap bg-primary text-black"
             href="/request-appointment"
