505254cfeb27a0971ea7ae7f89ac6073c1210975
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-12-25 18:26:26 +0500

    header

--- src/data/header_links.ts
+++ src/data/header_links.ts
@@ -1,11 +1,12 @@
 export const links = [
-    { href: '/made-to-measure-blinds', label: 'Blinds', id: 2 },
-    { href: '/made-to-measure-curtains', label: 'Curtains', id: 5 },
-    { href: '/shutters-range', label: 'Shutters', id: 9 },
-    { href: '/commercial', label: 'Commercial', id: 12 },
-    { href: '/gallery', label: 'Gallery' },
-    { href: '/estimator', label: 'Estimator' },
-    { href: '/blog', label: 'Blog' },
-    { href: '/about-us', label: 'About Us' },
-    { href: '/contact-us', label: 'Contact Us' },
-  ];
+  { href: '/made-to-measure-blinds', label: 'Blinds', id: 2 },
+  { href: '/made-to-measure-curtains', label: 'Curtains', id: 5 },
+  { href: '/shutters-range', label: 'Shutters', id: 9 },
+  { href: '#', label: 'Moterised',id: 20},
+  { href: '/commercial', label: 'Commercial', id: 12 },
+  { href: '/gallery', label: 'Gallery' },
+  { href: '/estimator', label: 'Estimator' },
+  { href: '/blog', label: 'Blog' },
+  { href: '/about-us', label: 'About Us' },
+  { href: '/contact-us', label: 'Contact Us' },
+];
