fff0def415bdd0cba0ef6fa4de6ba9557b6bd914
Wajid-Fareed <wajidfareed103@gmail.com>
2024-12-27 15:17:31 +0500

    meta tags added for missing pages and content added for about us

--- src/app/automated-curtains/page.tsx
+++ src/app/automated-curtains/page.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 import MotorisedPage from 'components/ui/MotorisedPage';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Automatic Window Curtains | Motorised Curtains | Smart Curtains Dubai',
+  description: 'We offer automatic window curtains in Dubai that combine style and convenience. Our motorised curtains offer light control, privacy, and a sleek design.',
+  openGraph: {
+    title: 'Automatic Window Curtains | Motorised Curtains | Smart Curtains Dubai',
+    description: 'We offer automatic window curtains in Dubai that combine style and convenience. Our motorised curtains offer light control, privacy, and a sleek design.',
+    url: 'https://b-c-eight.vercel.app/automated-curtains',
+    images: [
+      {
+        url: 'https://b-c-eight.vercel.app/blindsandcurtains.jpg',
+        alt: 'blindsandcurtains',
+      },
+    ],
+  },
+  alternates: {
+    canonical: 'https://b-c-eight.vercel.app/automated-curtains',
+  },
+};
 
 const MotorisedCurtains = () => {
   return (
