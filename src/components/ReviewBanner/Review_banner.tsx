a7a658a92e47d123478814b2f39e8723050c36b3
MFaad1 <faadsardar123@gmail.com>
2024-12-27 11:00:49 +0500

    meta title and description added for restuarant page

--- src/components/ReviewBanner/Review_banner.tsx
+++ src/components/ReviewBanner/Review_banner.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
 import Container from 'components/Res-usable/Container/Container';
 import Link from 'next/link';
+import {Client} from "@googlemaps/google-maps-services-js";
 
 function SampleNextArrow(props: any) {
   const { onClick } = props;
@@ -36,6 +37,8 @@ function SamplePrevArrow(props: any) {
     </div>
   );
 }
+
+const client = new Client({});
 
 interface Review {
   author_name: string;
