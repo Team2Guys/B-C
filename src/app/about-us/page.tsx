324f4dff41406a9f11ec1589856b511e64cd6867
Wajid-Fareed <wajidfareed103@gmail.com>
2025-01-02 11:24:47 +0500

    about us border fixed

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -77,27 +77,28 @@ const AboutUsPage = () => {
           ))} */}
           <div className="mt-5 pe-4 flex flex-col gap-4">
             {UsHistoryPara.paragraph.map((item, index) => (
-              <p key={index} className="leading-6 text-paralight mb-4 text-16" dangerouslySetInnerHTML={{ __html : item}}></p>
+              <p key={index} className="leading-6 text-paralight mb-4 text-16" dangerouslySetInnerHTML={{ __html: item }}></p>
             ))}
           </div>
         </div>
         <div className="flex flex-row gap-0 relative items-center sm::w-[85%] mx-auto w-full">
-          <div className="me-4 sm:me-14 h-5/6 border-dashed border border-btnclr hidden sm:block"></div>
+          <div className="me-5 sm:me-14 h-[81%] border border-dashed border-btnclr hidden sm:block  mt-7 xl:mt-5 2xl:mt-3" style={{
+            borderImage: "repeating-linear-gradient(135deg, #a9b4a4, #a9b4a4 10px, transparent 10px, transparent 20px) 1",
+          }}></div>
           <div className="flex flex-col">
             {OurHistoryData.map((item) => (
               <>
-                <div className="flex sm:hidden justify-center items-center w-16 xs:w-20 h-16 xs:h-20 rounded-full bg-btnclr text-white font-semibold">
-                  {item.year}
-                </div>
                 <div className="relative" key={item.id}>
                   <div className="absolute top-1/2 -left-14 -translate-y-1/2 sm:flex items-center hidden">
-                    <div className="border-dashed border border-btnclr w-4"></div>
-                    <div className="flex justify-center items-center w-16 xs:w-20 h-16 xs:h-20 rounded-full bg-btnclr text-white font-semibold">
+                    <div className="border-dashed border border-btnclr w-6"  style={{
+            borderImage: "repeating-linear-gradient(90deg, #a9b4a4, #a9b4a4 10px, transparent 10px, transparent 20px) 1",
+          }}></div>
+                    <div className="flex justify-center items-center w-16 xs:w-20 h-16 xs:h-20 rounded-full bg-btnclr text-white font-semibold tracking-widest">
                       {item.year}
                     </div>
                   </div>
                   <div className="bg-transparent hover:bg-white p-3 sm:p-6 xs:pt-10 pb-8 ps-2 sm:ps-16 pe-6 xs:pe-10 rounded-bl-[30px] rounded-tr-[30px]">
-                    <h3 className="text-18 xs:text-20 sm:text-24 leading-8 font-bold" dangerouslySetInnerHTML={{ __html : item.heading}}></h3>
+                    <h3 className="text-18 xs:text-20 sm:text-24 leading-8 font-bold" dangerouslySetInnerHTML={{ __html: item.heading }}></h3>
                     <p className="text-paralight font-normal leading-6 mt-2 text-12 xs:text-14 sm:text-normal">
                       {item.discription}
                     </p>
