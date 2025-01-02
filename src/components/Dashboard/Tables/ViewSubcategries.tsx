841e375412902f7eb9bd4b0691cb476fcee44122
Wajid-Fareed <wajidfareed103@gmail.com>
2025-01-02 14:49:17 +0500

    build fixed

--- src/components/Dashboard/Tables/ViewSubcategries.tsx
+++ src/components/Dashboard/Tables/ViewSubcategries.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Table, notification, Modal } from 'antd';
 import Image from 'next/image';
 import { RiDeleteBin6Line } from 'react-icons/ri';
