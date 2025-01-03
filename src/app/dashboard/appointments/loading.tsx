fba2038f8feb4f522e7e57c5d5a8ab0f765f1a21
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-01-03 18:00:53 +0500

    fix aapointment

---
+++ src/app/dashboard/appointments/loading.tsx
@@ -0,0 +1,13 @@
+'use client'
+import TableSkeleton from 'components/Dashboard/Tables/TableSkelton'
+import React from 'react'
+
+
+function loading() {
+  return (
+    <TableSkeleton rows={10} columns={1} />
+  )
+  
+}
+
+export default loading
