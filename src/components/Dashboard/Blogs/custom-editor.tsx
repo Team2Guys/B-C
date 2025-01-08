3d8293becdff31584a99c0ac0b87ea0bb69a10b2
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-01-08 11:20:46 +0500

    fixes: blog draft issue ⭐

--- src/components/Dashboard/Blogs/custom-editor.tsx
+++ src/components/Dashboard/Blogs/custom-editor.tsx
@@ -16,7 +16,7 @@ const MyEditor = ({
 }: any) => {
   const editorRef = useRef<any>(null);
   const [content, setContent] = useState(values.content || '');
-  
+
   useEffect(() => {
     setContent(values.content);
   }, [values.content]);
@@ -38,45 +38,104 @@ const MyEditor = ({
       content: newContent,
     });
   };
-  const config = useMemo(() => ({
-    height: 400,
-    menubar: true,
-    placeholder: placeholder || 'Start typing...',
-    file_picker_types: 'file image media',
-    automatic_uploads: true,
-    image_advtab: true,
-    image_dimensions: true,
-    image_caption: true,
-    image_title: true,
-    autosave_interval: '10s',
-    a11y_advanced_options: true,
-    quickbars_insert_toolbar: true,
-    nonbreaking_force_tab: true,
-    allow_html_in_named_anchor: true,
-    quickbars_image_toolbar: 'alignleft aligncenter alignright | rotateleft rotateright | imageoptions',
-    plugins: [
-      'advlist','blocks fontfamily fontsize', 'autolink','accordion','quickbars','autosave','importword','restoredraft','upload', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
-      'searchreplace', 'wordcount', 'visualblocks','insertimage', 'visualchars', 'code', 'fullscreen', 'insertdatetime','quickbars image editimage',
-      'media', 'table', 'emoticons', 'help','directionality','nonbreaking'
-    ],
-    toolbar1: 'undo redo|blocks fontfamily fontsize | styles |restoredraft|nonbreaking| bold italic|accordion|ltr rtl |quickimage|quicktable| importword  ',
-     toolbar2: 'alignleft aligncenter alignright alignjustify |bullist numlist outdent indent | link image| print preview media fullscreen |forecolor backcolor emoticons| anchor|restoredraft | help',
-    menu: {
-      file: { title: 'File', items: 'newdocument restoredraft | preview | importword exportpdf exportword | print | deleteallconversations' },
-      edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
-      view: { title: 'View', items: 'code revisionhistory | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments' },
-      insert: { title: 'Insert', items: 'image link media addcomment pageembed codesample inserttable | upload image |upload | math | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime' },
-      format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat' },
-      tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount' },
-      table: { title: 'Table', items: 'inserttable | cell row column | advtablesort | tableprops deletetable' },
-      help: { title: 'Help', items: 'help' }
+  const config = useMemo(
+    () => ({
+      height: 400,
+      menubar: true,
+      placeholder: placeholder || 'Start typing...',
+      file_picker_types: 'file image media',
+      automatic_uploads: true,
+      image_advtab: true,
+      image_dimensions: true,
+      image_caption: true,
+      image_title: true,
+      autosave_interval: '10s',
+      a11y_advanced_options: true,
+      quickbars_insert_toolbar: true,
+      nonbreaking_force_tab: true,
+      allow_html_in_named_anchor: true,
+      quickbars_image_toolbar:
+        'alignleft aligncenter alignright | rotateleft rotateright | imageoptions',
+      plugins: [
+        'advlist',
+        'blocks fontfamily fontsize',
+        'autolink',
+        'accordion',
+        'quickbars',
+        'autosave',
+        'importword',
+        'restoredraft',
+        'upload',
+        'link',
+        'image',
+        'lists',
+        'charmap',
+        'preview',
+        'anchor',
+        'pagebreak',
+        'searchreplace',
+        'wordcount',
+        'visualblocks',
+        'insertimage',
+        'visualchars',
+        'code',
+        'fullscreen',
+        'insertdatetime',
+        'quickbars image editimage',
+        'media',
+        'table',
+        'emoticons',
+        'help',
+        'directionality',
+        'nonbreaking',
+      ],
+      toolbar1:
+        'undo redo|blocks fontfamily fontsize | styles |restoredraft|nonbreaking| bold italic|accordion|ltr rtl |quickimage|quicktable| importword  ',
+      toolbar2:
+        'alignleft aligncenter alignright alignjustify |bullist numlist outdent indent | link image| print preview media fullscreen |forecolor backcolor emoticons| anchor|restoredraft | help',
+      menu: {
+        file: {
+          title: 'File',
+          items:
+            'newdocument restoredraft | preview | importword exportpdf exportword | print | deleteallconversations',
+        },
+        edit: {
+          title: 'Edit',
+          items:
+            'undo redo | cut copy paste pastetext | selectall | searchreplace',
+        },
+        view: {
+          title: 'View',
+          items:
+            'code revisionhistory | visualaid visualchars visualblocks | spellchecker | preview fullscreen | showcomments',
+        },
+        insert: {
+          title: 'Insert',
+          items:
+            'image link media addcomment pageembed codesample inserttable | upload image |upload | math | charmap emoticons hr | pagebreak nonbreaking anchor tableofcontents | insertdatetime',
+        },
+        format: {
+          title: 'Format',
+          items:
+            'bold italic underline strikethrough superscript subscript codeformat | styles blocks fontfamily fontsize align lineheight | forecolor backcolor | language | removeformat',
+        },
+        tools: {
+          title: 'Tools',
+          items: 'spellchecker spellcheckerlanguage | a11ycheck code wordcount',
+        },
+        table: {
+          title: 'Table',
+          items:
+            'inserttable | cell row column | advtablesort | tableprops deletetable',
+        },
+        help: { title: 'Help', items: 'help' },
       },
       contextmenu: [
         'cut copy paste | link image inserttable | image | tableprops deletetable | cell row column',
         'bold italic underline | removeformat | forecolor backcolor',
-        'insertdatetime emoticons | code'
+        'insertdatetime emoticons | code',
       ],
-      file_picker_callback: (cb: (url: string, meta: any) => void,) => {
+      file_picker_callback: (cb: (url: string, meta: any) => void) => {
         const input = document.createElement('input');
         input.setAttribute('type', 'file');
         input.setAttribute('accept', 'image/*');
@@ -86,9 +145,10 @@ const MyEditor = ({
             const reader = new FileReader();
             reader.onload = () => {
               const id = 'blobid' + new Date().getTime();
-              const blobCache = window.tinymce.activeEditor?.editorUpload.blobCache;
+              const blobCache =
+                window.tinymce.activeEditor?.editorUpload.blobCache;
               const base64 = reader.result?.toString().split(',')[1];
-              
+
               if (base64 && blobCache) {
                 const blobInfo = blobCache.create(id, file, base64);
                 blobCache.add(blobInfo);
@@ -98,11 +158,12 @@ const MyEditor = ({
             reader.readAsDataURL(file);
           }
         };
-      
+
         input.click();
       },
-      
-    }), [placeholder]);
+    }),
+    [placeholder],
+  );
 
   return (
     <Editor
@@ -111,7 +172,13 @@ const MyEditor = ({
       value={content}
       init={config}
       onBlur={(evt, editor) => handleBlur(editor.getContent())}
-      onEditorChange={(newContent) => handleContentChange(newContent)}
+      onEditorChange={(newContent) => {
+        handleContentChange(newContent);
+        handleDebouncedMutation({
+          ...values,
+          content: newContent,
+        });
+      }}
     />
   );
 };
