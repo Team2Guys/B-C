c7615583fefdd160e5a7905b74e29382c310a863
Wajid-Fareed <wajidfareed103@gmail.com>
2025-01-13 17:47:32 +0500

    meta tags & description

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -11,16 +11,16 @@ import { Metadata } from 'next';
 
 
 export const metadata: Metadata = {
-  title: 'About Us Page',
-  description: 'About Us description',
+  title: 'Blinds and Curtains Dubai | Made to Measure Blinds | About Us',
+  description: 'Learn all about Blinds and Curtains Dubai. We make custom blinds and curtains just for you. Stylish, high-quality, and perfect for your home or office.',
   openGraph: {
-    title: 'About Us',
-    description: 'About Us description',
+    title: 'Blinds and Curtains Dubai | Made to Measure Blinds | About Us',
+    description: 'Learn all about Blinds and Curtains Dubai. We make custom blinds and curtains just for you. Stylish, high-quality, and perfect for your home or office.',
     url: 'fullUrl',
     images: [
       {
         url: 'imageUrl',
-        alt: 'altText',
+        alt: 'Blinds and Curtains Dubai | Made to Measure Blinds | About Us',
       },
     ],
   },
