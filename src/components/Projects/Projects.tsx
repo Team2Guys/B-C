643ade5756d602f834f3ce8fdf1a747088c25b58
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-01-14 12:14:58 +0500

    remove heading specing for small screen

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,7 +7,7 @@ import { projectFont } from 'typo/font';
 const Projects: React.FC = () => {
   const sortedProjects = projectsData.sort((a, b) => a.title.localeCompare(b.title));
   return (
-    <Container className="px-6 pt-12">
+    <Container className="px-6 pt-2 md:pt-12">
       <p
         className={` md:mx-20 line leading-10 md:leading-[54px] text-center text-2xl md:text-[38px] mb-8 ${projectFont.className}`}
       >
