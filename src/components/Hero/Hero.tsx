fc7172ce7de4a31534cd06e78aaf5673372cd411
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-01-21 16:02:21 +0500

    fixes: Snaglist changes⚡

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -13,7 +13,7 @@ function Hero() {
   return (
     <Container>
       <h1 className="hidden">Blinds & Curtains Dubai</h1>
-      <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center h-[75vh] xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
+      <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center min-h-[60svh]  xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
         {heroSlider.map((item: any) => {
           return (
             <div key={item.id}>
