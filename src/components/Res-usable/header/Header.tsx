25504fe403f76a60277b4ffa98fa555a78a5fe76
MFaad1 <faadsardar123@gmail.com>
2025-01-22 19:14:38 +0500

    fixes redirect url issues

--- src/components/Res-usable/header/Header.tsx
+++ src/components/Res-usable/header/Header.tsx
@@ -3,10 +3,7 @@ import Navbar from './Navbar'
 import { fetchProducts, fetchSubCategories } from 'config/fetch';
 
 const Header = async () => {
-  const [products, subCategories] = await Promise.all([
-        fetchProducts(),
-        fetchSubCategories(),
-      ]);
+  const [products, subCategories] = await Promise.all([fetchProducts(),fetchSubCategories()]);
   return (
     <Navbar products={products}  subCategories={subCategories} />
   )
