07f4916b24ebb9616fabc42030127d1aa7e33224
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-01-23 10:27:40 +0500

    fixes: remove _ to space e.g Wooden_blinds to Wooden Blinds

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -16,16 +16,24 @@ export function getStatusNameByCode(code: number): string | undefined {
 
   export function capitalizeWords(input: any): any {
     if (Array.isArray(input)) {
-        // Handle array input: capitalize each word and return the updated array
-        return input.map(word => word.trim().charAt(0).toUpperCase() + word.trim().slice(1));
+        return input.map(word =>
+            word
+                .split('_') 
+                .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+                .join(' ') 
+        );
     } else if (typeof input === 'string') {
-        // Handle string input: split by commas, capitalize, and return as an array
         return input
             .split(',')
-            .map(word => word.trim().charAt(0).toUpperCase() + word.trim().slice(1));
+            .map(word =>
+                word
+                    .split('_')
+                    .map(part => part.charAt(0).toUpperCase() + part.slice(1)) 
+                    .join('') 
+            );
     } else {
-        // Handle unexpected types gracefully
         return input;
     }
 }
+
 
