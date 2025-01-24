1c75fa17affd11d8f3a973664638cd3004e07180
MFaad1 <faadsardar123@gmail.com>
2025-01-24 09:35:19 +0500

    fixes

--- src/components/Hero/HeroSlide.tsx
+++ src/components/Hero/HeroSlide.tsx
@@ -70,6 +70,7 @@ const HeroSlide = ({ item }: any) => {
                         <Image
                             className="w-full h-full object-cover max-sm:object-contain"
                             loading='lazy'
+                            
                             width={600}
                             height={600}
                             alt={item.name}
