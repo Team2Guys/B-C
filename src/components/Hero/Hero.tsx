b863b37cebf19bb7691ce517010351e1c065bfb2
MFaad1 <faadsardar123@gmail.com>
2025-01-24 16:51:16 +0500

    fixes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,14 @@
 import Container from 'components/Res-usable/Container/Container';
-import CustomSlider from 'components/slider/Slider';
+// import CustomSlider from 'components/slider/Slider';
 import { heroSlider } from 'data/data';
 import React, { Fragment, } from 'react';
 import HeroSlide from './HeroSlide';
+import dynamic from 'next/dynamic'
+const CustomSlider = dynamic(() => import('components/slider/Slider'), {
+  loading: () => <p>Loading...</p>,
+})
+
+
 function Hero() {
   return (
     <Container>
@@ -10,10 +16,7 @@ function Hero() {
       <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center min-h-[60svh]  xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
         {heroSlider.map((item: any) => {
           return (
-            <Fragment key={item.id}>
               <HeroSlide item={item} />
-            </Fragment>
-
           );
         })}
       </CustomSlider>
