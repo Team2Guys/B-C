890d2ba5ffcddd59f9832c2d501cce770555682c
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2025-01-25 15:43:37 +0500

    refactor: try to improve accessibility

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -12,7 +12,7 @@ function Hero() {
     <Container>
       <h1 className="hidden">Blinds & Curtains Dubai</h1>
 
-      <div  className="min-h-[60svh] flex justify-center items-center">
+      <div className="min-h-[60svh] flex justify-center items-center">
 
         <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center min-h-[60svh] xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
           {heroSlider.map((item: any, index: number) => (
