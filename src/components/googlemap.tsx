ebd9d5093526deac239b9127dfb405081ebc343b
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-25 17:51:51 +0500

    sldier fixed

--- src/components/googlemap.tsx
+++ src/components/googlemap.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 function GoogleMap() {
   return (
     <GoogleMapsEmbed
+      id='footermap'
       apiKey={process.env.NEXT_PUBLIC_REVIEWS_API_KEY || ""}
       height={200}
       width="100%"
