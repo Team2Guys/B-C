f579a8c093e80bb79a68599f5b7ea314681b88a3
MFaad1 <faadsardar123@gmail.com>
2025-01-25 14:34:45 +0500

    fixes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,10 +2,8 @@ import Container from 'components/Res-usable/Container/Container';
 import { heroSlider } from 'data/data';
 import HeroSlide from './HeroSlide';
 import dynamic from 'next/dynamic'
-// import { Suspense } from 'react';
-import HeroSkeleton from './HeroSkelton';
 const CustomSlider = dynamic(() => import('components/slider/Slider'), {
-  loading: () => <HeroSkeleton />,
+  loading: () => <div>loading...</div>,
 })
 
 
@@ -13,9 +11,7 @@ function Hero() {
   return (
     <Container>
       <h1 className="hidden">Blinds & Curtains Dubai</h1>
-      {/* <Suspense fallback={<div className="min-h-[60svh] flex justify-center items-center">
-        <HeroSkeleton />
-      </div>}> */}
+
       <div  className="min-h-[60svh] flex justify-center items-center">
 
         <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center min-h-[60svh] xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
@@ -26,7 +22,6 @@ function Hero() {
         </CustomSlider>
 
       </div>
-      {/* </Suspense> */}
     </Container>
   );
 }
