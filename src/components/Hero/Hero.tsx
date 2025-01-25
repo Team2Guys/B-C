faf0bb33663648fc63be0510588de811d37b8961
MFaad1 <faadsardar123@gmail.com>
2025-01-25 14:27:23 +0500

    fixes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,10 @@
-'use client'
 import Container from 'components/Res-usable/Container/Container';
 import { heroSlider } from 'data/data';
 import HeroSlide from './HeroSlide';
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 import HeroSkeleton from './HeroSkelton';
 const CustomSlider = dynamic(() => import('components/slider/Slider'), {
-  ssr: false,
   loading: () => <HeroSkeleton />,
 })
 
@@ -15,15 +13,20 @@ function Hero() {
   return (
     <Container>
       <h1 className="hidden">Blinds & Curtains Dubai</h1>
-      <Suspense fallback={<div className="min-h-[60svh] flex justify-center items-center">
+      {/* <Suspense fallback={<div className="min-h-[60svh] flex justify-center items-center">
         <HeroSkeleton />
-      </div>}>
+      </div>}> */}
+      <div  className="min-h-[60svh] flex justify-center items-center">
+
         <CustomSlider className="Hero-slider z-40 pb-3 xl:pt-0 2xl:pt-10 content-center min-h-[60svh] xs:h-full md:h-[55vh] lg:h-[65vh] xl:h-[80vh] 2xl:h-[84vh]">
           {heroSlider.map((item: any, index: number) => (
             <HeroSlide item={item} key={index} />
           ))}
+
         </CustomSlider>
-      </Suspense>
+
+      </div>
+      {/* </Suspense> */}
     </Container>
   );
 }
