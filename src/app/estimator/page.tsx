85092358902b8257a644fbd0bea5682b882be5e9
MFaad1 <faadsardar123@gmail.com>
2025-01-28 14:29:45 +0500

    fixes

--- src/app/estimator/page.tsx
+++ src/app/estimator/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { fetchProducts } from 'config/fetch';
 import EstimatorPage from './Estimator';
-import { allowedTitles, predefinedOrder } from 'data/urls';
-import { estimator_data } from 'data/data';
 import { Metadata } from 'next';
+import { estimatorprod } from 'data/estimator';
 
 export const metadata: Metadata = {
   title: 'Estimator | Price Calculator | Blinds And Curtains Dubai',
@@ -27,24 +25,9 @@ export const metadata: Metadata = {
 };
 
 const Estimator: React.FC = async () => {
-  const products = await fetchProducts();
 
-  const filteredFetchedProducts = products
-    ? products.filter((product: any) => allowedTitles.includes(product.title))
-    : [];
-
-  const allProducts = [...estimator_data, ...filteredFetchedProducts];
-  const sortedProducts = allProducts.sort((a, b) => {
-    const aIndex = predefinedOrder.indexOf(a.title);
-    const bIndex = predefinedOrder.indexOf(b.title);
-    if (aIndex === -1 || bIndex === -1) {
-      return aIndex === -1 ? 1 : -1;
-    }
-
-    return aIndex - bIndex;
-  });
   return (
-    <EstimatorPage products={products} sortedProducts={sortedProducts} />
+    <EstimatorPage sortedProducts={estimatorprod}  />
   );
 };
 
---
+++ src/data/estimator.tsx
@@ -0,0 +1,62 @@
+export const estimatorprod = [
+  {
+    id: 1,
+    title: 'Roller Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/roller.webp' },
+    price: 100,
+  },
+  {
+    id: 49,
+    title: 'Roman Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/roman.webp' },
+    price: 300,
+  },
+  {
+    id: 27,
+    title: 'Vertical Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/vertical.webp' },
+    price: 100,
+  },
+  {
+    id: 166,
+    title: 'Wooden Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/wooden.webp' },
+    price: 300,
+  },
+  {
+    id: 56,
+    title: 'Aluminium Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/aluminum.webp' },
+    price: 90,
+  },
+  {
+    id: 129,
+    title: 'Zebra Blinds',
+    posterImage: { imageUrl: '/assets/images/estimator/Zebra.webp' },
+    price: 250,
+  },
+  {
+    id: 25,
+    title: 'Blackout Curtains',
+    posterImage: { imageUrl: '/assets/images/estimator/blackout.webp' },
+    price: 375,
+  },
+  {
+    id: 41,
+    title: 'Sheer Curtains',
+    posterImage: { imageUrl: '/assets/images/estimator/r1r70qlj26z7gof19pny.webp' },
+    price: 275,
+  },
+  {
+    id: 2,
+    title: 'Shutters MDF',
+    posterImage: { imageUrl: '/assets/images/estimator/white1.webp' },
+    price: 700,
+  },
+  {
+    id: 3,
+    title: 'Shutters Basswood',
+    posterImage: { imageUrl: '/assets/images/estimator/white2.webp' },
+    price: 900,
+  },
+]
