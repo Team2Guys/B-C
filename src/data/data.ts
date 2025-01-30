4223a0e5f550303ecda82b4e956d3d1126040591
Wajid-Fareed <wajidfareed103@gmail.com>
2025-01-30 18:18:45 +0500

    hero slide images added

--- src/data/data.ts
+++ src/data/data.ts
@@ -66,22 +66,22 @@ export const heroSlider = [
   {
     id: 1,
     name: 'g2.png',
-    imageUrl: '/assets/images/Hero/g2.png',
+    imageUrl: '/assets/images/Hero/g1.png',
     heading: 'The Home of Customised',
     subheading: 'Window Coverings',
     content:"Our beautiful collection of window covering isn't the only thing that makes us stand out. A lot of it has to do with how we treat you right from the moment you contact us to the moment your project is complete."  },
   {
     id: 2,
-    name: 'g3.webp',
-    imageUrl: '/assets/images/Hero/g3.webp',
+    name: 'g3.png',
+    imageUrl: '/assets/images/Hero/g3.png',
     heading: 'Made To Measure Window  ',
     subheading: 'Curtains In Dubai',
     content:"Finding a reliable curtains company in Dubai is no easy task, but Blinds and Curtains make it simple. You can choose from 1000s of fabrics and colours - velvets, silks, and plain textures.",
   },
   {
     id: 3,
-    name: 'g1.webp',
-    imageUrl: '/assets/images/Hero/g1.webp',
+    name: 'g1.png',
+    imageUrl: '/assets/images/Hero/g2.png',
     heading: 'Perfect Fit Window',
     subheading: 'Shutters Dubai',
     content:
