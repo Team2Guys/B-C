691ccbed57b616e7f17495fe0db2fd646c6c4d05
MFaad1 <faadsardar123@gmail.com>
2025-04-18 12:53:19 +0500

    script optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,26 +47,14 @@ export default function RootLayout({
 
 
         </Head>
-
-             {/* Google Tag Manager */}
-             <Script
-            strategy='afterInteractive'
-              id="google-tag-manager"
-              dangerouslySetInnerHTML={{
-                __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MNXTN5B');
-            `,
-              }}
-            />
+        <GoogleTagManager gtmId="GTM-MNXTN5B"  />
 
             {/* Microsoft Clarity */}
             <Script
             
               id="clarity-script"
-     strategy='afterInteractive'
+              strategy="lazyOnload"
+
               dangerouslySetInnerHTML={{
                 __html: `
               (function(c,l,a,r,i,t,y){
@@ -80,7 +68,7 @@ export default function RootLayout({
         {/* Meta Pixel */}
         <Script
               id="meta-pixel"
-              strategy='afterInteractive'
+              strategy="lazyOnload"
               dangerouslySetInnerHTML={{
                 __html: `
               !function(f,b,e,v,n,t,s)
@@ -98,11 +86,15 @@ export default function RootLayout({
             />
             {/* Google Analytics Script */}
         <Script
-        strategy="afterInteractive"
+         strategy="lazyOnload"
+
         src="https://www.googletagmanager.com/gtag/js?id=G-2W1CWBHDRB"
       />
       
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" 
+   strategy="lazyOnload"
+
+      >
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -114,6 +106,8 @@ export default function RootLayout({
 
 
         <body className={` ${gotham.className} bg-lightgrey`}>
+
+
           <GoogleTagManager gtmId="GTM-MNXTN5B" />
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNXTN5B"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
