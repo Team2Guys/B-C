02a85e8d9c1e474499caff2e8e07a79ced909f1e
MFaad1 <faadsardar123@gmail.com>
2025-04-23 10:05:27 +0500

    fixes

--- src/app/blinds/[product]/[subproduct]/Subproduct.tsx
+++ src/app/blinds/[product]/[subproduct]/Subproduct.tsx
@@ -9,7 +9,6 @@ interface ISubProduct { products: IProduct[],
 
 
 const SubProduct = ({ products, filteredProduct, filteredSubCategory }:ISubProduct ) => {
-console.log(filteredSubCategory, "filteredSubCategory")
   return (
     <>
       {filteredSubCategory ? (
