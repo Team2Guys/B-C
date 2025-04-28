8b9dc092f06de0d7cfc97b1dc17916d0947c52a9
MFaad1 <faadsardar123@gmail.com>
2025-04-28 17:58:33 +0500

    fixes

--- src/components/motorised-blinds/motorised-info.tsx
+++ src/components/motorised-blinds/motorised-info.tsx
@@ -32,7 +32,7 @@ const MotorisedInfo: React.FC<MotorisedInfoProps> = ({
       <div className={`flex flex-wrap lg:flex-nowrap mb-2 lg:gap-10 ${className}`}>
         <div className="w-full lg:w-/12 xl:w-6/12">
           <div className="space-y-2">
-            <h2 className="text-32 md:text-[41px] font-medium text-center sm:text-start">{title || ""}</h2>
+            <p className="text-32 md:text-[41px] font-medium text-center sm:text-start">{title || ""}</p>
             <p className="underline underline-offset-8 text-[#6F747F] text-center sm:text-start">
               {subtitle || ""}
             </p>
