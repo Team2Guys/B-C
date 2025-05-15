e8d41618e5c5e5bca14f4f2a91f83a431fb2e4e1
misbahkhalil07 <misbahperveenpaf@gmail.com>
2025-05-15 17:59:09 +0500

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ import { Roboto_Serif } from 'next/font/google';
 
 export const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ['100', '300', '400', '500', '700', '900'],
     variable: '--font-roboto',
   display: 'swap',
 });
---
+++ src/data/Homedata/tabdata.ts
@@ -0,0 +1,43 @@
+//   Tabdata
+  export const tabData = [
+    {
+      title: "Expert Guidance",
+      icon: "/assets/Home/tab/export.webp", 
+      image: "/assets/Home/tab/trusted.webp", 
+      heading: "Expert Guidance at Your Doorstep",
+      description:
+        "Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions.Discover what makes us the preferred choice for window solutions.",
+      buttonText: "Book A Free Visit",
+      href:"/request-appointment"
+    },
+    {
+      title: "24/7 Support",
+      icon: "/assets/Home/tab/high-quality.webp",
+       image: "/assets/Home/tab/trusted.webp", 
+      heading: "Expert Guidance at Your Doorstep1",
+      description:
+        "Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions.Discover what makes us the preferred choice for window solutions.",
+      buttonText: "Book A Free Visit",
+        href:"/request-appointment"
+    },
+    {
+      title: "Stress-Free Experience",
+      icon: "/assets/Home/tab/stress1.webp",
+      image: "/assets/Home/tab/trusted.webp", 
+      heading: "Expert Guidance at Your Doorstep2",
+      description:
+        "Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions.Discover what makes us the preferred choice for window solutions.",
+      buttonText: "Book A Free Visit",
+       href:"/request-appointment"
+    },
+    {
+      title: "In-House Production",
+      icon: "/assets/Home/tab/support.webp",
+      image: "/assets/Home/tab/trusted.webp", 
+      heading: "Expert Guidance at Your Doorstep3",
+      description:
+        "Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions. Discover what makes us the preferred choice for window solutions.Discover what makes us the preferred choice for window solutions.",
+      buttonText: "Book A Free Visit",
+       href:"/request-appointment"
+    },
+  ];
